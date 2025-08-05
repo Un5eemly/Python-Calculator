3319e00af711feef2072e5cc9d8779fb6ce6e935
Un5eemly <Un5eemly@proton.me>
2025-08-05 08:39:32 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,19 +4,19 @@ if operation.lower() == "add" or operation.lower() == "addition" or operation.lo
        number1 = input("Enter the first number: ")
        number2 = input("Enter the second number: ")
        YourAnswer =int(number1) + int(number2)
-       print(YourAnswer)
+       print("YourAnswer is: ", (YourAnswer))
 
 elif operation.lower() == "subtract" or operation.lower() == "subtraction" or operation.lower() == "minus" or operation.lower() == "-":     
        number1 = input("Enter the first number: ")
        number2 = input("Enter the second number: ")
        YourAnswer =int(number1) - int(number2)
-       print(YourAnswer)
+       print("YourAnswer is: ", (YourAnswer))
 
 elif operation.lower() == "multiply" or operation.lower() == "multiplicaiton" or operation.lower() == "into" or operation.lower() == "x" or operation.lower() == "*":     
        number1 = input("Enter the first number: ")
        number2 = input("Enter the second number: ")
        YourAnswer =int(number1) * int(number2)
-       print(YourAnswer)
+       print("YourAnswer is: ", (YourAnswer))
 
 elif operation.lower() == "divide" or operation.lower() == "division" or operation.lower() == "/":     
        number1 = input("Enter the first number: ")
